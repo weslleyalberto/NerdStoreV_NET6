538226df4d66cc4e5a0e9ba511851a522e7d7694
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-06 18:20:59 -0300

    Adicionado classe para gerenciar dimensoes com as devidas validações

---
+++ src/NerdStore.Catalogo.Domain/Dimensoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    internal class Dimensoes
+    {
+    }
+}

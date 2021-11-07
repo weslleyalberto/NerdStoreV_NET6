fd3a3816b80c41734e17a5f81c56e4e6d4a3bc53
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-07 20:50:16 -0300

    Adicionado ao projeto , camada de WebApplications

---
+++ src/NerdStore.WebApps.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NerdStore.WebApps.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

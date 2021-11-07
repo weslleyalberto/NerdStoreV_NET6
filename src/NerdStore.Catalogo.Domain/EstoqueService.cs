83fb752f606e98341693d94d265e2c022bc636ce
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-07 18:39:59 -0300

    Implementado serviço de evento utilizando o pacote mediart para Estoque Service

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,11 +1,16 @@
-﻿namespace NerdStore.Catalogo.Domain
+﻿using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+
+namespace NerdStore.Catalogo.Domain
 {
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
-        public EstoqueService(IProdutoRepository produtoRepository)
+        private readonly IMediatrHandler _bus;
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository; 
+            _bus = bus;
         }
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
         {
@@ -19,6 +24,12 @@
                 return false;
             }
             produto.DebitarEstoque(quantidade);
+            //TODO - Parametrizar a quantidade de estoque baixo
+            if(produto.QuantidadeEstoque < 10)
+            {
+                //avisar , email , chamado , realizar nova compra 
+               await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produtoId,produto.QuantidadeEstoque));
+            }
             _produtoRepository.Atualizar(produto);
             return await _produtoRepository.UnitOfWork.Commit();
         }
---
+++ src/NerdStore.Core/Messages/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Message
+    {
+        public string MessageType { get; protected set; }
+        public Guid AggragateId { get; protected set; }
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+
+   
+}

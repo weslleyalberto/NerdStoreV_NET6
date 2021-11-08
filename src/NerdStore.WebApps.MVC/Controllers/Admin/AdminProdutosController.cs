162b1b5e37d717b329380f01d0f30add2dfa9952
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-08 00:20:14 -0300

    Criada as Views para Atualizar, Listar e Cadastra Produtos

--- src/NerdStore.WebApps.MVC/Controllers/Admin/AdminProdutosController.cs
+++ src/NerdStore.WebApps.MVC/Controllers/Admin/AdminProdutosController.cs
@@ -5,6 +5,7 @@ using NerdStore.Catalogo.Domain;
 
 namespace NerdStore.WebApps.MVC.Controllers.Admin
 {
+    
     public class AdminProdutosController : Controller
     {
         private readonly IProdutoAppService _produtoAppService;
@@ -24,14 +25,36 @@ namespace NerdStore.WebApps.MVC.Controllers.Admin
         {
             return View(await PopularCategorias(new ProdutoViewModel()));
         }
-        [Route("novo-produto")]
         [HttpPost]
+        [Route("novo-produto")]       
         public async Task<IActionResult> NovoProduto(ProdutoViewModel produtoViewModel)
         {
             if (!ModelState.IsValid) return View(await PopularCategorias(produtoViewModel));
             await _produtoAppService.AdicionarProduto(produtoViewModel);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        [Route("produtos-atualizar-estoque")]  
+        public async Task<IActionResult> AtualizarEstoque(Guid id)
+        {
+            return View("Estoque", await _produtoAppService.ObterPorId(id));
+        }
+        [HttpPost]
+        [Route("Produtos-atualizar-estoque")]
+        public async Task<IActionResult> AtualizarEstoque(Guid id,int quantidade)
+        {
+            if(quantidade > 0)
+            {
+                await _produtoAppService.ReporEstoque(id, quantidade);  
+            }
+            else
+            {
+                await _produtoAppService.DebitarEstoque(id,quantidade);
+            }
+            return View("Index", await _produtoAppService.ObterTodos());
+        }
+
+
         private async Task<ProdutoViewModel> PopularCategorias(ProdutoViewModel produtoViewModel)
         {
             produtoViewModel.Categorias = await _produtoAppService.ObterPorCategorias();

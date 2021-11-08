7be17f246014a301c4e094fef536c620dd144576
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-07 22:46:37 -0300

    Implantado Controller para Admininistração de Produtos

---
+++ src/NerdStore.WebApps.MVC/Controllers/Admin/AdminProdutosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.WebApps.MVC.Controllers.Admin
+{
+    public class AdminProdutosController : Controller
+    {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public AdminProdutosController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+        [HttpGet]
+        [Route("admin-produtos")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _produtoAppService.ObterTodos());
+        }
+        [Route("novo-produto")]
+        public async Task<IActionResult> NovoProduto()
+        {
+            return View(await PopularCategorias(new ProdutoViewModel()));
+        }
+        [Route("novo-produto")]
+        [HttpPost]
+        public async Task<IActionResult> NovoProduto(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) return View(await PopularCategorias(produtoViewModel));
+            await _produtoAppService.AdicionarProduto(produtoViewModel);
+            return RedirectToAction("Index");
+        }
+        private async Task<ProdutoViewModel> PopularCategorias(ProdutoViewModel produtoViewModel)
+        {
+            produtoViewModel.Categorias = await _produtoAppService.ObterPorCategorias();
+            return produtoViewModel;
+        }
+
+    }
+}

c431235ec79af15605ad80d7431e9a691b92af1e
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-09 23:06:41 -0300

    Alterada IProdutoAppService com a exclusao do método de onbter categorias por id(GUID)

--- src/NerdStore.Catalogo.Domain/IProdutoRepository.cs
+++ src/NerdStore.Catalogo.Domain/IProdutoRepository.cs
@@ -8,9 +8,10 @@ namespace NerdStore.Catalogo.Domain
         Task<Produto> ObterPorId(Guid id);
         Task<IEnumerable<Produto>> ObterPorCategoria(int codigo);
         Task<IEnumerable<Categoria>> ObterCategorias();
-        Task<Categoria> ObterCategoriasPorId(Guid id);
+
         void Adicionar(Produto produto);
-        void Atualizar(Produto produto);    
+        void Atualizar(Produto produto);
+
         void Adicionar(Categoria categoria);
         void Atualizar(Categoria categoria);
     }

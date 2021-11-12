582ec3762196b5cdae9e07b725da5d67e3a677d8
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-11 22:22:04 -0300

    Criada enum para obter o Status do Pedido

--- src/NerdStore.Vendas.Domain/PedidoStatus.cs
+++ src/NerdStore.Vendas.Domain/PedidoStatus.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NerdStore.Vendas.Domain
+﻿namespace NerdStore.Vendas.Domain
 {
     public enum PedidoStatus
     {
+        Rascunho = 0,
+        Iniciado =1,
+        Pago =4,
+        Entregue = 5,
+        Cancelado = 6
     }
 }

e2ce0ba5e9de166fbd2ac35121780c4674ef6c3e
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-11 21:15:11 -0300

    Adicionada camada de Domain no contexto de vendas

---
+++ src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,17 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem :Entity
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        //EF relation
+        public Pedido Pedido { get; set; }
+
+    }
+}
